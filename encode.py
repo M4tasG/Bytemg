546077bb15d00deb22cf85db86a0491eab0f92b0
M4tasG <grybauskas.matas@gmail.com>
2021-04-22 13:13:57 +0300

    encoding done

--- encode.py
+++ encode.py
@@ -1,5 +1,11 @@
 from PIL import Image, ImageDraw
 
+def split_byte_to_nums(text):
+    num_list = []
+    for char in text:
+        num_list.append(int(char))
+    return num_list
+
 def convert_to_binary(byte_array):
     byte_list = []
     for byte in byte_array:
@@ -7,11 +13,37 @@ def convert_to_binary(byte_array):
         byte_list.append(byte)
     return byte_list
 
+def parse_byte_to_str(byte_list):
+    parsed_list = []
+    for byte in byte_list:
+        byte = str(byte).replace('b','')
+        parsed_list.append(byte)
+    return parsed_list
+
+def draw_bytes(byte_list):
+    img = Image.new('1', (8, len(text)))
+    #draw = ImageDraw.Draw(img)
+    x = 0
+    y = 0
+    #i = 0
+    for byte in byte_list:
+        split_byte = split_byte_to_nums(byte)
+        for byte in split_byte:
+            print(byte)
+            if(byte):
+                img.putpixel((x,y), 0)
+            else:
+                img.putpixel((x,y), 1)
+            x += 1
+        x = 0
+        y += 1
+    img.save('encoded.jpg')
+
 text = input()
 text = convert_to_binary(bytearray(text, "utf8"))
+text = parse_byte_to_str(text)
+draw_bytes(text)
 
-img = Image.new('1', (8, len(text)))
-draw = ImageDraw.Draw(img)
 
 
 print(text)
